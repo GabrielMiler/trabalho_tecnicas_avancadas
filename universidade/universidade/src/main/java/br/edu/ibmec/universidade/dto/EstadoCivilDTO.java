6acb5945c469b2770ce35369ac91810b63033afc
GabrielMiler <142256207+GabrielMiler@users.noreply.github.com>
2025-10-09 20:27:58 -0300

    Delete universidade/universidade directory

--- universidade/universidade/src/main/java/br/edu/ibmec/universidade/dto/EstadoCivilDTO.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ibmec.universidade.dto;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement
-public enum EstadoCivilDTO {
-	solteiro, casado, divorciado, viuvo;
-}
--- universidade/universidade/src/main/java/br/edu/ibmec/universidade/entity/EstadoCivil.java
+++
@@ -1,6 +0,0 @@
-package br.edu.ibmec.universidade.entity;
-
-
-public enum EstadoCivil {
-	solteiro, casado, divorciado, viuvo;
-}
